693d438718679822062c6fc6eda3061b9dd48176
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-05-13 22:39:13 +0200

    wip

--- rotate_test.lua
+++ rotate_test.lua
@@ -4,25 +4,15 @@ require_mod("moreblocks")
 
 local function slice(ctx, opts)
     local fn = opts.full_node
-    local an = "air"
     local s1 = { name=opts.slope_node, param2=21 } -- x+, y-
     local s2 = { name=opts.slope_raised_node, param2=21 }
     local pn = opts.pane_node
-    local po = opts.pane_offset_node
 
     ctx
     :pattern({
         {fn, fn, fn},
         {fn, s2, s1},
         {fn, pn, pn}
-    })
-
-    ctx
-    :translate(0, 0, -1)
-    :pattern({
-        {an, an, an},
-        {an, po, po},
-        {an, an, an}
     })
 end
 
@@ -56,16 +46,10 @@ return function(ctx, opts)
         full_node = "scifi_nodes:blackdmg",
         slope_node = "scifi_nodes:slope_blackdmg_half",
         slope_raised_node = "scifi_nodes:slope_blackdmg_half_raised",
-        pane_node = "scifi_nodes:octgrn_pane",
-        pane_offset_node = "scifi_nodes:octgrn_pane_offset",
+        pane_node = "scifi_nodes:octgrn_pane"
     }, opts)
 
-    for xi=0,1 do
-        for yi=0,1 do
-            ctx
-            :rotate(90, 0, 0)
-            :translate((3*(yi%2))+(xi*6),5*yi,0)
-            :execute(window, opts)
-        end
-    end
+    ctx
+    :rotate(90, 0, 90)
+    :execute(window, opts)
 end
