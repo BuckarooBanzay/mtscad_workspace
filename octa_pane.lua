073568b69325887f4c3bee6517aad3709df355ce
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-06-12 11:40:21 +0200

    fill wip

--- octa_pane.lua
+++ octa_pane.lua
@@ -23,12 +23,48 @@ local function quarter(ctx, opts)
     :translate(0, opts.border-1, 0)
     :line(opts.border-1, -opts.border+1, 0)
 
-    -- inner slopes
+    if opts.fill then
+        -- fill window
+        -- TODO
+    else
+        -- inner slopes
+        ctx
+        :with(opts.slope)
+        :slope(1, 1, 0)
+        :translate(1, opts.border, 0)
+        :line(opts.border-1, -opts.border+1, 0)
+    end
+end
+
+local function apply_rotate_quarters(ctx, opts)
+    -- rotate and apply for each quarter
+    opts = mtscad.merge({
+        fn = function() end,
+        height = 10,
+        width = 10,
+        opts = {}
+    }, opts)
+
+    assert(opts.width % 2 == 0, "width should be an even number")
+    assert(opts.height % 2 == 0, "height should be an even number")
+
     ctx
-    :with(opts.slope)
-    :slope(1, 1, 0)
-    :translate(1, opts.border, 0)
-    :line(opts.border-1, -opts.border+1, 0)
+    :execute(opts.fn, opts.opts)
+
+    ctx
+    :translate(opts.width-1, 0, 0)
+    :rotate(0, 0, 90)
+    :execute(opts.fn, opts.opts)
+
+    ctx
+    :translate(0, opts.height-1, 0)
+    :rotate(0, 0, 270)
+    :execute(opts.fn, opts.opts)
+
+    ctx
+    :translate(opts.width-1, opts.height-1, 0)
+    :rotate(0, 0, 180)
+    :execute(opts.fn, opts.opts)
 end
 
 return function(ctx, opts)
@@ -37,29 +73,14 @@ return function(ctx, opts)
         width = 16,
         border = 5,
         node = "default:stone",
+        fill = false,
         slope = "moreblocks:slope_stone"
     }, opts)
 
-    assert(opts.width % 2 == 0, "width should be an even number")
-    assert(opts.height % 2 == 0, "height should be an even number")
-
-    -- rotate and apply for each quarter
-    ctx
-    :execute(quarter, opts)
-
-    ctx
-    :translate(opts.width-1, 0, 0)
-    :rotate(0, 0, 90)
-    :execute(quarter, opts)
-
-    ctx
-    :translate(0, opts.height-1, 0)
-    :rotate(0, 0, 270)
-    :execute(quarter, opts)
-
-    ctx
-    :translate(opts.width-1, opts.height-1, 0)
-    :rotate(0, 0, 180)
-    :execute(quarter, opts)
-
+    ctx:execute(apply_rotate_quarters, {
+        opts = opts,
+        fn = quarter,
+        width = opts.width,
+        height = opts.height
+    })
 end
